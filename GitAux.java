681a92f170237824a327bc4664a653baa7cf0108
JoaquinCardoso <Joaquin.informatica@alu.uma.es>
2013-03-13 16:54:16 +0100

    modificaciones alumno 1

--- GitAux.java
+++ GitAux.java
@@ -12,6 +12,7 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+	System.out.println("modificado por alum1"); // insertado por alum1
     }
     
     public void metodoAlumno2(){
@@ -21,6 +22,8 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
    }
    public void testGitaux(){
    	// metodo rama testing
