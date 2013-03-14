c04c6ea82ced253db82b2ab533a2f5f79afa95d5
alejimgar89 <alejimgar89@uma.es>
2013-03-14 11:54:21 +0100

    Modificaciones colaborador

--- GitAux.java
+++ GitAux.java
@@ -16,11 +16,13 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("Modificaciones hechas por alumno2"); //modif alumno2
    	//Linea modificada
    }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        System.out.println("Modificaciones hechas por alumno2"); //modif alumno2
    }
    public void testGitaux(){
    	// metodo rama testing
